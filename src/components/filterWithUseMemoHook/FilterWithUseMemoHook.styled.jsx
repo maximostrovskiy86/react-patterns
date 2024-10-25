105db8f3e909bc348939811bd7fe06081d6013c2
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-10-25 12:48:01 +0200

    feat: added  Filter component with useMemo hook

---
+++ src/components/filterWithUseMemoHook/FilterWithUseMemoHook.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const FilterWrapper = styled.label`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin-top: 10px;
+
+    input {
+        margin-bottom: 10px;
+    }
+`
